1a41a7ecb304dd42a1cd30b6cf01f91d55c43ecc
José Antônio <jasaj.ti@gmail.com>
2025-11-17 19:42:56 -0300

    Ajuste da tabela pagameno

--- src/main/java/br/senac/rj/backend/entity/Pagamento.java
+++ src/main/java/br/senac/rj/backend/entity/Pagamento.java
@@ -5,8 +5,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -21,12 +19,11 @@ import lombok.Data;
 public class Pagamento {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Integer id_pagamento;
 
-    @Column(length = 80, nullable = false)
-    private String nome;
+    @Column(length = 50, nullable = false)
+    private String forma_pagamento;
 
-    @ManyToOne
-    @JoinColumn(name = "turma_id")
-    private Turma turma;
+    @Column(length = 50, nullable = false)
+    private String detalhe_pagamento;
 }
